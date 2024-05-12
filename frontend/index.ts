c0ddee2b2bd03459bc98b55784c7ce14ecc52c34
Sitota <sirichosiro@gmail.com>
2024-05-12 06:40:54 +0300

    learn larvel

---
+++ frontend/index.ts
@@ -0,0 +1,10 @@
+import { ArrayOfObjectsInput } from "sanity";
+import skill from "./skill";
+import testimonials from "./testimonials";
+import about  from "./about"
+import brands from "./brands";
+import contact from "./contact";
+import experince from "./experince";
+import works from "./works";
+import workExperince from "./workExperince";
+ export const schemaTypes = [testimonials,skill,about,brands,contact,experince,works,workExperince]
